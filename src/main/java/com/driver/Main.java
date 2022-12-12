2eede5f72419221b6897d307d550fea95434052e
Abhish'k <abhishek@192.168.1.4>
2022-12-12 11:16:22 +0530

    hii

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,12 +1,14 @@
 package com.driver;
 
+
+
 public class Main {
-    public static class Product{
-        public int product(int x,int y)
-        {
-            return x+y;
+    public static class Product {
+        public int product(int x, int y) {
+            return x + y;
 
         }
+
         public int product(int x,int y,int z)
         {
             return x+y+z;
@@ -25,9 +27,10 @@ public class Main {
     public static void main(String[] args)
     {
         Product obj=new Product();
-        obj.product(3,4);
-        obj.product(3,4,5);
-        obj.product(33.00,44.00);
+        int ans1=obj.product(3,4);
+        int ans2 =obj.product(3,4,5);
+      double ans3= obj.product(33.00,44.00);
+        System.out.println(ans1 +" "+ans2+" "+ans3);
 
 
     }
